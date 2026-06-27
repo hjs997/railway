2100ed8c2642f87c390f491ad95f843e67a3629e
hjs997 <74714688+hjs997@users.noreply.github.com>
2026-06-27 21:06:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ const PORT = process.env.SERVER_PORT || process.env.PORT || 3000;        // http
 const UUID = process.env.UUID || '9afd1229-b893-40c1-84dd-51e7ce204913'; // 使用哪吒v1,在不同的平台运行需修改UUID,否则会覆盖
 const NEZHA_SERVER = process.env.NEZHA_SERVER || 'sui.uuz.us.kg:443';        // 哪吒v1填写形式: nz.abc.com:8008  哪吒v0填写形式：nz.abc.com
 const NEZHA_PORT = process.env.NEZHA_PORT || '';            // 使用哪吒v1请留空，哪吒v0需填写
-const NEZHA_KEY = process.env.NEZHA_KEY || '';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
+const NEZHA_KEY = process.env.NEZHA_KEY || 'T5tS7ogr7RdD4IuIW93BL8wHs5d1PB5N';              // 哪吒v1的NZ_CLIENT_SECRET或哪吒v0的agent密钥
 const ARGO_DOMAIN = process.env.ARGO_DOMAIN || 'way.uuz.us.kg';          // 固定隧道域名,留空即启用临时隧道
 const ARGO_AUTH = process.env.ARGO_AUTH || 'eyJhIjoiNTQzZDRkZTQzYjBkMjFhY2I0OTgyMmJkZGI1NzdkOTQiLCJ0IjoiZmJjM2NiMGItNzU4Yi00NzVkLTkzYjUtYzUwZTA5YjRlYWU0IiwicyI6Ik16RXpNMkpoTURFdE5UTXlNaTAwTWpJNUxUa3dOVEV0TjJGa1l6UXdPV0V4TVdGaCJ9';              // 固定隧道密钥json或token,留空即启用临时隧道,json获取地址：https://json.zone.id
 const ARGO_PORT = process.env.ARGO_PORT || 8001;            // 固定隧道端口,使用token需在cloudflare后台设置和这里一致
-const CFIP = process.env.CFIP || 'cdns.doon.eu.org';        // 节点优选域名或优选ip  
+const CFIP = process.env.CFIP || 'saas.sin.fan';        // 节点优选域名或优选ip  
 const CFPORT = process.env.CFPORT || 443;                   // 节点优选域名或优选ip对应的端口
 const NAME = process.env.NAME || 'ray';                        // 节点名称
 
